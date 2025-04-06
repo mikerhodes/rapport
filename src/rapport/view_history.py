9fcdd03fe7fb2a86192b42d1ae78e40d1476ef36
Mike Rhodes <mike.rhodes@gmail.com>
2025-04-06 14:24:41 +0100

    feat: nicer history display

--- src/rapport/view_history.py
+++ src/rapport/view_history.py
@@ -32,6 +32,15 @@ def load_chat(chat_id):
 
 
 def main():
+    st.html("""
+        <style>
+        div[class*="st-key-chat_chathistory-"]{}
+         button {
+            justify-content: left !important;
+            text-align: left;
+        }
+        </style>
+        """)
     st.title("History")
     recent_chats = st.session_state["history_manager"].get_recent_chats(
         limit=100
@@ -40,21 +49,24 @@ def main():
 
 
 def show_chat_history(recent_chats: List[Dict]):
+    day = None
     for chat in recent_chats:
+        if day != chat["created_at"][:7]:
+            st.subheader(chat["created_at"][:7])
+            day = chat["created_at"][:7]
+
         col1, col2 = st.columns([6, 1])
         with col1:
             # Highlight current chat
-            icon = None
             current_chat = st.session_state.get("chat", None)
-            if current_chat and chat["id"] == current_chat.id:
-                icon = ":material/edit:"
+            is_current_chat = current_chat and chat["id"] == current_chat.id
             st.button(
                 chat["title"],
                 key=f"chat_chathistory_{chat['id']}",
                 on_click=load_chat,
                 args=(chat["id"],),
                 use_container_width=True,
-                icon=icon,
+                type="primary" if is_current_chat else "tertiary",
             )
         with col2:
             st.button(
